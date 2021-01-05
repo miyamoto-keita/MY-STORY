81b87638eb924eda6f8a7f3cb6d344a6920ab845
miyamoto-keita <70692500+miyamoto-keita@users.noreply.github.com>
2021-01-05 14:08:47 +0900

    コメント機能
    コメントしたり削除したりする

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
+  # @post.comments
   validates :title, presence: true, length: { minimum: 3, message: 'To short to post!'}
   validates :body, presence: true
 end
